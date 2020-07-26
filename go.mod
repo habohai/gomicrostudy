781d8fcbe5b09950c05f12f429f2a98e8d9a579b
haibeihabo <haibei0408@icloud.com>
2020-07-26 15:09:33 +0800

    go-micro学习 gin web框架加入

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/haibeihabo/gomicrostudy
 go 1.14
 
 require (
+	github.com/gin-gonic/gin v1.6.3 // indirect
 	github.com/micro/go-micro v1.18.0
 	github.com/micro/micro/v2 v2.9.3 // indirect
 )
--- main.go
+++ main.go
@@ -1,19 +1,25 @@
 package main
 
 import (
-	"net/http"
-
+	"github.com/gin-gonic/gin"
 	"github.com/micro/go-micro/web"
 )
 
 func main() {
+	ginRouter := gin.Default()
+
+	ginRouter.Handle("GET", "/user", func(context *gin.Context) {
+		context.JSON(200, "user api")
+	})
+
+	ginRouter.Handle("GET", "/news", func(context *gin.Context) {
+		context.JSON(200, "news api")
+	})
+
 	server := web.NewService(
 		web.Address(":9060"),
+		web.Handler(ginRouter),
 	)
-
-	server.HandleFunc("/", func(write http.ResponseWriter, request *http.Request) {
-		write.Write([]byte("hello world"))
-	})
 
 	server.Run()
 }

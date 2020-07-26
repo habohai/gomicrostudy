d8d1efd719bc9df511af8751a4524000af7845ca
haibeihabo <haibei0408@icloud.com>
2020-07-26 21:06:32 +0800

    go-micro学习 consul

--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.14
 
 require (
 	github.com/gin-gonic/gin v1.6.3 // indirect
-	github.com/micro/go-micro v1.18.0
+	github.com/micro/go-plugins/registry/consul v0.0.0-20200119172437-4fe21aa238fd // indirect
 	github.com/micro/micro/v2 v2.9.3 // indirect
 )
--- main.go
+++ main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+
+	"github.com/micro/go-micro/registry"
 	"github.com/micro/go-micro/web"
+	"github.com/micro/go-plugins/registry/consul"
 )
 
 func main() {
+	reg := consul.NewRegistry(
+		registry.Addrs("192.168.31.82:8500"),
+	)
+
 	ginRouter := gin.Default()
 
 	ginRouter.Handle("GET", "/user", func(context *gin.Context) {
@@ -17,8 +24,10 @@ func main() {
 	})
 
 	server := web.NewService(
+		web.Name("prodservice"),
 		web.Address(":9060"),
 		web.Handler(ginRouter),
+		web.Registry(reg),
 	)
 
 	server.Run()
